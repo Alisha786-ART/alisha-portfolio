f852c99083523a8c841cffbee810761aa050b391
Alisha Tamboli <alishatamboli0786@gmail.com>
2025-02-14 22:51:09 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Portfolio from "./components/Portfolio";
+;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Portfolio />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
